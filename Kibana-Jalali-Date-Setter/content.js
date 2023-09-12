e961d338e614ae22fc9753ee83025fbca4ff240b
Mehran <skullboxmhrn@gmail.com>
2023-09-12 15:50:46 +0430

    Initial Commit

---
+++ Kibana-Jalali-Date-Setter/content.js
@@ -0,0 +1,114 @@
+// Function to convert Persian numerals to Latin
+function persianToLatin(persianNum) {
+  const persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+  const latin = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  for (let i = 0; i < persian.length; i++) {
+    const regex = new RegExp(persian[i], "g");
+    persianNum = persianNum.replace(regex, latin[i]);
+  }
+  return persianNum;
+}
+
+// Function to insert the input elements
+function insertInputs() {
+  const dashboardViewport = document.querySelector('.dashboardViewport');
+
+  if (dashboardViewport) {
+    const containerDiv = document.createElement('div');
+    containerDiv.style.marginBottom = '20px';
+    containerDiv.style.display = 'flex';
+    containerDiv.style.flexDirection = 'column';
+    containerDiv.style.alignItems = 'center';
+    containerDiv.style.border = '1px solid black';
+    containerDiv.style.borderRadius = '5px';
+
+    const instruction = document.createElement('div');
+    instruction.innerText = 'لطفا بازه زمانی مورد نظر خود را انتخاب کنید';
+    containerDiv.appendChild(instruction);
+    instruction.style.marginTop = '10px'
+    instruction.style.fontSize = '15px'
+
+    const lineBreak = document.createElement('br');
+    containerDiv.appendChild(lineBreak);
+
+    const div = document.createElement('div');
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.marginBottom = '5px'
+
+    const fromDateLabel = document.createElement('span');
+    fromDateLabel.innerText = 'از تاریخ:';
+    div.appendChild(fromDateLabel);
+
+    const fromDateInput = document.createElement('input');
+    fromDateInput.type = 'text';
+    fromDateInput.id = 'fromDateInput';
+    fromDateInput.placeholder = 'jYYYY/jMM/jDD';
+    fromDateInput.style.borderRadius = '4px';
+    fromDateInput.style.margin = '0 10px';
+    div.appendChild(fromDateInput);
+
+    const toDateLabel = document.createElement('span');
+    toDateLabel.innerText = 'تا تاریخ:';
+    div.appendChild(toDateLabel);
+
+    const toDateInput = document.createElement('input');
+    toDateInput.type = 'text';
+    toDateInput.id = 'toDateInput';
+    toDateInput.placeholder = 'jYYYY/jMM/jDD';
+    toDateInput.style.borderRadius = '4px';
+    toDateInput.style.margin = '0 10px';
+    div.appendChild(toDateInput);
+
+    const setDateButton = document.createElement('button');
+    setDateButton.innerText = 'انتخاب تاریخ';
+    setDateButton.style.borderRadius = '4px';
+    setDateButton.style.border = '1px solid black';
+    setDateButton.style.padding = '4px'
+    div.appendChild(setDateButton);
+
+    containerDiv.appendChild(div);
+    dashboardViewport.insertBefore(containerDiv, dashboardViewport.firstChild);
+
+    // Initialize PikadayJalali
+    new Pikaday({
+      field: document.getElementById('fromDateInput'),
+      isJalaali: true
+    });
+
+    new Pikaday({
+      field: document.getElementById('toDateInput'),
+      isJalaali: true
+    });
+
+    setDateButton.addEventListener('click', function () {
+      // Convert Persian numbers to Latin and get the date strings
+      const fromDate = persianToLatin(fromDateInput.value);
+      const toDate = persianToLatin(toDateInput.value);
+
+      // Create ISO 8601 date strings
+      const fromDateISO = new Date(fromDate).toISOString();
+      const toDateISO = new Date(toDate).toISOString();
+
+      // Extract the current URL
+      const currentUrl = window.location.href;
+
+      // Parse current URL to extract dashboard ID and other parts
+      const hashParts = currentUrl.split('#');
+      const hashSubParts = hashParts[1].split('?');
+      const dashboardId = hashSubParts[0].split('/view/')[1];
+
+      // Create a new URL with the updated time range
+      const newUrl = `https://your-domain.com/app/dashboards#/view/${dashboardId}?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'${encodeURIComponent(fromDateISO)}',to:'${encodeURIComponent(toDateISO)}'))`;
+
+      // Redirect to the new URL
+      window.location.href = newUrl;
+    });
+
+    // Clear the interval once the element is found and inputs are inserted
+    clearInterval(interval);
+  }
+}
+
+// Periodically check for the element and insert the inputs once it's available
+const interval = setInterval(insertInputs, 1000);
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Kibana-Jalali-Date-Setter-Extention
-Kibana Jalali Date Setter Extention For Chromium
+# Kibana Jalali Date Setter Extention
+
+## Description
+
+This is a Chromium browser extension designed to set the date range for a Kibana dashboard using the Jalali (Persian) calendar. This extension uses Moment.js, Moment-Jalali, and Pikaday-Jalali to manage date functionality. Users can also style the date picker with their own CSS.
+
+![Extension Screenshot](./screenshot.png)
+
+## Prerequisites
+
+- Chromium-based browser (Google Chrome, Brave, Microsoft Edge, etc.)
+- Access to a Kibana dashboard
+
+## Libraries Used
+
+- [Moment.js](https://momentjs.com/)
+- [Moment-Jalali](https://github.com/jalaali/moment-jalaali)
+- [Pikaday-Jalali](https://github.com/TheServat/Pikaday-Jalali)
+
+## Installation
+
+1. Clone the repository to your local machine.
+2. Open the Chromium browser and navigate to `chrome://extensions/`.
+3. Enable 'Developer Mode' if it's not already enabled.
+4. Click on the 'Load Unpacked' button and select the directory where you cloned the repository.
+
+## Configuration
+
+1. Open the `manifest.json` and `content.js` files.
+2. Replace all instances of `your-domain.com` with your Kibana dashboard domain name.
+
+## Custom Styling
+
+To apply your own styles to the date picker, you can modify the `styles.css` file.
+
+## Usage
+
+1. Install and configure the extension.
+2. Navigate to your Kibana dashboard.
+3. You should see a new date picker panel at the top of the dashboard viewport.
+4. Set your desired date range in the Jalali calendar.
+5. Click on 'Set Date' to apply the new date range to your Kibana dashboard.
+
+## License
+
+This project is licensed under the GNU v3.0. See the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Contributions are welcome! Create a new GitHub issue for bug reports, feature requests, or questions. Open a pull request to contribute improvements or fixes.
+
+## Authors
+
+[Mehran Goudarzi](https://github.com/your-github-username)
+[Kooshan Ghelman](https://github.com/your-github-username)
